39630370e5db8e6e16b9424dd2bb7c0cbc8f2c9f
lzw <liangzhw21@lzu.edu.cn>
2023-06-01 08:51:02 +0800

    initial commit

---
+++ 模块/module.py
@@ -0,0 +1,2 @@
+import 加减乘除 as jjcc
+print(jjcc.myadd(1, 2))
---
+++ 模块/加减乘除.py
@@ -0,0 +1,9 @@
+def myadd(a, b):
+    return a + b
+def mysub(a, b):
+    return a - b
+def mymul(a, b):
+    return a * b
+def mydiv(a, b):
+    return a / b
+print("hello world")
